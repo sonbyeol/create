3f53dba968e7474ae063ba81905616ffe53dc2c4
sonbyeol <50646247+sonbyeol@users.noreply.github.com>
2024-04-15 06:39:20 +0900

    Update usersrest.tsx

--- usersrest.tsx
+++ usersrest.tsx
@@ -2,6 +2,23 @@
 import { useMediaQuery, Theme } from "@mui/material";
 import { List, SimpleList, Datagrid, TextField, Create, SimpleForm, ReferenceInput, TextInput } from "react-admin";
 import MyUrlField from './MyUrlField';
+import './usersrest.css';
+
+export const Userrest = () => {
+    return (
+        <div className="dashboard-container">
+          <div className="box">
+            <h2>Box 1</h2>
+            <UserrestList />
+          </div>
+          <div className="box">
+            <h2>Box 2</h2>
+            <p>This is the content of Box 2.</p>
+          </div>
+        </div>
+      );
+};
+
 export const UserrestList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
     return (
